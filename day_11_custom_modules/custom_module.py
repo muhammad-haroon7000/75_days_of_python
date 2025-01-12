886387a6d5769adf5a24100cb9a25a2f2d986ffa
muhammad-haroon7000 <muhammadharoon7000@gmail.com>
2025-01-12 10:31:56 +0500

    Day 11 modules in Python

---
+++ day_11_custom_modules/custom_module.py
@@ -0,0 +1,15 @@
+
+def Add (x,y):
+    """ Add two numbers """
+    return (x +y)
+
+def greet(name, title='Mr.'):
+    """ Greet a person by name with title
+    Args:
+        name: The first argument.
+        title: The second argument.
+    
+    Hello, {title} {name}
+    """
+    print(f"Hello, {title} {name}")
+    
